d5ca64f6ef154457d7c50e7120b9044ca2ab2d84
Sukrut28 <sukrutchinchalkar1@gmail.com>
2025-11-03 00:27:41 +0530

    small

--- Roshara-Frontend/app/products/[id]/page.jsx
+++ Roshara-Frontend/app/products/[id]/page.jsx
@@ -3,7 +3,6 @@
 import { useEffect, useState, useMemo } from "react";
 import { useParams } from "next/navigation";
 import { useCart } from "../../../context/CartContext";
-import { getProductById } from "../../../services/productService";
 
 const SIZE_CHART = {
   "6XS": { bust: 22, waist: 15, hips: 25, shoulder: 13 },
