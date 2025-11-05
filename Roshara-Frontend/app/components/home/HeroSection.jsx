8ea858c06d8a7b50f88f016a18e93da252649a4a
Sejal Mastoli <mastolisejal7@gmail.com>
2025-11-06 01:24:13 +0530

    a

--- Roshara-Frontend/app/components/home/HeroSection.jsx
+++ Roshara-Frontend/app/components/home/HeroSection.jsx
@@ -166,7 +166,7 @@ export default function HeroSection() {
           <Image
             src="/assets/Tarni_Front1.jpg"
             alt="Right Model"
-            width={600}
+            width={570}
             height={750}
             className="object-contain"
           />
@@ -193,7 +193,7 @@ export default function HeroSection() {
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 1 }}
-          className="absolute right-[29rem] top-[0rem] z-10"
+          className="absolute right-[29rem] top-[6rem] z-10"
         >
           <Image
             src="/assets/ROSHARA.png"
